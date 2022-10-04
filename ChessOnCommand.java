ef021755ee12f3c090a254c1a6e01afac822d2de
AndreasOstlund04 <97974684+AndreasOstlund04@users.noreply.github.com>
2022-10-04 20:45:51 +0200

    Multiple functions and refactoring

--- ChessOnCommand.java
+++ ChessOnCommand.java
@@ -1,50 +1,62 @@
 import java.io.IOException;
 import java.util.Scanner;
 
+//Todo
+//Long algebraic chess notation
+//K for king, Q for queen, R for rook, B for bishop, N for knight, empty space for pawn
+
 public class ChessOnCommand {
+
+    //the board represented with a two dimensional string array
+    static String[][]chessboard = new String[8][8];
+
     public static void main (String []args ) throws InterruptedException{
 
         //control variables
         int menuState = 0;
+        int currentTurn = 0;
 
-        //the board represented with a two dimensional string array
-        String chessboard[][];
+        //settings
+        int verticalBorderSize = 8;
+        int horizontalBorderSize = 16;
 
         Scanner input = new Scanner(System.in);
 
         ClearConsole();
-        TitlePrint();
-
-
-        System.out.println("1. Play\n");
-        System.out.println("2. Help\n");
-        System.out.println("3. Exit\n");
+        verticalBorderPrint(verticalBorderSize);
+        TitlePrint(horizontalBorderSize);
+        System.out.println();
+        System.out.println();
+        horizontalBorderPrint(horizontalBorderSize);
+        System.out.println("                           1. Play");
+        System.out.println();
+        horizontalBorderPrint(horizontalBorderSize);
+        System.out.println("                           2. Help");
+        System.out.println();
+        horizontalBorderPrint(horizontalBorderSize);
+        System.out.println("                           3. Exit\n");
         
         while(menuState == 0){
 
             switch(intInput()) { 
                 case 1: 
                     ClearConsole();
-                    break; 
+                    resetBoard();
+                    printBoard(horizontalBorderSize, verticalBorderSize);
+                break; 
                 case 2: 
                     ClearConsole();
                     //slap the rules in here
                     System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
                     System.out.println("\n Enter to continue");
-                    break; 
+                break; 
                 case 3: 
                     ClearConsole();
                     System.out.println( "Goodbye world");
-                    Thread.sleep(5000);
+                    Thread.sleep(4000);
                     System.exit(0);
-                    break; 
-    
+                break; 
+
             }
         }
         
@@ -66,20 +78,318 @@ public class ChessOnCommand {
         }
     }
 
-    public static void TitlePrint(){
+    public static void TitlePrint(int horizontalBorderSize){
 
+        horizontalBorderPrint(horizontalBorderSize);
         System.out.println("_________  .__");
+        horizontalBorderPrint(horizontalBorderSize);
         System.out.println("\\_   ___ \\ |  |__    ____    ______  ______    ____    ____");
+        horizontalBorderPrint(horizontalBorderSize);
         System.out.println("/    \\  \\/ |  |  \\ _/ __ \\  /  ___/ /  ___/   /  _ \\  /    \\");
+        horizontalBorderPrint(horizontalBorderSize);
         System.out.println("\\     \\____|   Y  \\  ___/   \\___ \\  \\___ \\   (  <_> )|   |  \\");
+        horizontalBorderPrint(horizontalBorderSize);
         System.out.println(" \\______  /|___|  / \\___  >/____  >/____  >   \\____/ |___|\n");
+        horizontalBorderPrint(horizontalBorderSize);
         System.out.println("                                                      .___");
+        horizontalBorderPrint(horizontalBorderSize);
         System.out.println("  ____    ____    _____    _____  _____     ____    __| _/");
+        horizontalBorderPrint(horizontalBorderSize);
         System.out.println("_/ ___\\  /  _ \\  /     \\  /     \\ \\__  \\   /    \\  / __ | ");
+        horizontalBorderPrint(horizontalBorderSize);
         System.out.println("\\  \\___ (  <_> )|  Y Y  \\|  Y Y  \\ / __ \\_|   |  \\/ /_/ | ");
+        horizontalBorderPrint(horizontalBorderSize);
         System.out.println(" \\___  > \\____/ |__|_|  /|__|_|  /(____  /|___|  /\\____ | ");
+        horizontalBorderPrint(horizontalBorderSize);
         System.out.println("     \\/               \\/       \\/      \\/      \\/      \\/ ");
     }    
+
+    static void clearBoard(){
+
+        for(int i = 0; i<8; i++){
+
+            for(int i2 = 0; i2<8; i2++){
+            
+                chessboard[i][i2] = "Clear";
+            }   
+        }
+    }
+
+    static void resetBoard(){
+
+        for(int vertical = 0; vertical<8; vertical++){
+
+            for(int horizontal = 0; horizontal<8; horizontal++){
+
+                switch(vertical){
+
+                    case 0:
+                    switch (horizontal){
+
+                        case 0:
+                        chessboard[vertical][horizontal] = "Black Rook";  
+                        break;
+                        case 1:
+                        chessboard[vertical][horizontal] = "Black Knight";
+                        break;
+                        case 2:
+                        chessboard[vertical][horizontal] = "Black Bishop";
+                        break;
+                        case 3:
+                        chessboard[vertical][horizontal] = "Black Queen";
+                        break;
+                        case 4:
+                        chessboard[vertical][horizontal] = "Black King";
+                        break;
+                        case 5:
+                        chessboard[vertical][horizontal] = "Black Bishop";
+                        break;
+                        case 6:
+                        chessboard[vertical][horizontal] = "Black Knight";
+                        break;
+                        case 7:
+                        chessboard[vertical][horizontal] = "Black Rook";
+                        break;
+
+                    }
+                    break;
+                    case 1:
+                    chessboard[vertical][horizontal] = "Black Pawn";
+                    break;
+                    case 2:
+                    chessboard[vertical][horizontal] = "Clear";
+                    break;
+                    case 3:
+                    chessboard[vertical][horizontal] = "Clear";
+                    break;
+                    case 4:
+                    chessboard[vertical][horizontal] = "Clear";
+                    break;
+                    case 5:
+                    chessboard[vertical][horizontal] = "Clear";
+                    break;
+                    case 6:
+                    chessboard[vertical][horizontal] = "White Pawn";
+                    break;
+                    case 7:
+                    switch (horizontal){
+
+                        case 0:
+                        chessboard[vertical][horizontal] = "White Rook";  
+                        break;
+                        case 1:
+                        chessboard[vertical][horizontal] = "White Knight";
+                        break;
+                        case 2:
+                        chessboard[vertical][horizontal] = "White Bishop";
+                        break;
+                        case 3:
+                        chessboard[vertical][horizontal] = "White Queen";
+                        break;
+                        case 4:
+                        chessboard[vertical][horizontal] = "White King";
+                        break;
+                        case 5:
+                        chessboard[vertical][horizontal] = "White Bishop";
+                        break;
+                        case 6:
+                        chessboard[vertical][horizontal] = "White Knight";
+                        break;
+                        case 7:
+                        chessboard[vertical][horizontal] = "White Rook";
+                        break;
+
+                    }
+                    break;
+
+                }
+            }   
+        }
+    }
+
+    static void verticalBorderPrint(int verticalBorderSize){
+
+        //goes brrrrrrr
+        for(int i = 0; i < verticalBorderSize; i++){
+
+            System.out.println();
+
+        }
+    }
+
+    static void horizontalBorderPrint(int horizontalBorderSize){
+
+        //also goes brrrrrrr
+        for(int i = 0; i < horizontalBorderSize; i++){
+
+            System.out.print(" ");
+
+        }
+
+    }
+
+    static void printBoard(int horizontalBorderSize, int verticalBorderSize){
+
+        int length = 5;
+        verticalBorderPrint(verticalBorderSize);
+
+        for(int vertical = 0; vertical<8; vertical++){
+
+            if(vertical == 0){
+
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX");
+            }
+
+            for(int horizontal = 0; horizontal<8; horizontal++){
+
+                if(horizontal == 0){
+                    
+                horizontalBorderPrint(horizontalBorderSize);
+                }
+
+
+                length = chessboard[vertical][horizontal].length();
+
+                if(vertical % 2 == 0){
+
+                    if(horizontal % 2 == 0){
+
+                        System.out.print(".. ");
+                        System.out.print(chessboard[vertical][horizontal]);
+
+                        for(int i = length;i < 12; i++){
+                            System.out.print(" ");
+                        }
+
+                        System.out.print(" ..");
+
+                    } else {
+
+                        System.out.print("XX ");
+                        System.out.print(chessboard[vertical][horizontal]);
+
+                        for(int i = length;i < 12; i++){
+                            System.out.print(" ");
+                        }
+
+                        System.out.print(" XX");
+
+                    }
+
+                } else{
+
+                    if(horizontal % 2 == 0){
+
+                        System.out.print("XX ");
+                        System.out.print(chessboard[vertical][horizontal]);
+
+                        for(int i = length;i < 12; i++){
+                            System.out.print(" ");
+                        }
+
+                        System.out.print(" XX");
+
+                    } else {
+
+                        System.out.print(".. ");
+                        System.out.print(chessboard[vertical][horizontal]);
+
+                        for(int i = length;i < 12; i++){
+                            System.out.print(" ");
+                        }
+
+                        System.out.print(" ..");
+
+                    }
+                }
+            }//end of "horizontal" for loop
+
+            System.out.println();
+            switch(vertical){
+                case 0:
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..");
+                break;
+                case 1:
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX");
+                break;
+                case 2:
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..");
+                break;
+                case 3:
+                horizontalBorderPrint(horizontalBorderSize);
+                
+                System.out.println("XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX");
+                break;
+                case 4:
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..");
+                break;
+                case 5:
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX");
+                break;
+                case 6:
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..");
+                break;
+                case 7:
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XX              XX..              ..XX              XX..              ..XX              XX..              ..XX              XX..              ..");
+                horizontalBorderPrint(horizontalBorderSize);
+                System.out.println("XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................XXXXXXXXXXXXXXXXXX..................");
+                break;
+            }
+
+        }//end of "vertical" for loop
+    }
 
     static int intInput(){
         //Takes input and checks if integer
@@ -90,8 +400,6 @@ public class ChessOnCommand {
         
 
         while(tempControl == 0){
-
-            System.out.print(":");
 
             if(input.hasNextLine()){
 
